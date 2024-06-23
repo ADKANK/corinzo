f6f309c2aa653db5d1e16b8adc0510c82971f8e9
ADKANK <32964280+ADKANK@users.noreply.github.com>
2024-06-24 02:47:57 +0530

    Improved NavBar

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,41 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  constructor(private router: Router) { }
-  navigateToAbout() {
-    this.router.navigate(['/about']);
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.closeNavbar();
+      }
+    });
+  }
+  isNavbarCollapsed = false;
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
   }
 
-  navigateToProjects() {
-    this.router.navigate(['/projects']);
-  }
 
-  navigateToProducts() {
-    this.router.navigate(['/products']);
-  }
-
-  navigateToRailings() {
-    this.router.navigate(['/railings']);
-  }
-
-  navigateToBrochures() {
-    this.router.navigate(['/brochures']);
-  }
-
-  navigateToContact() {
-    this.router.navigate(['/contact']);
-  }
-
-  navigateToHome() {
-    this.router.navigate(['/home']);
+  closeNavbar() {
+    const navbarToggler = document.querySelector('.navbar-toggler') as HTMLElement;
+    if (navbarToggler.getAttribute('aria-expanded') === 'true') {
+      navbarToggler.click();
+    }
   }
 
 }
